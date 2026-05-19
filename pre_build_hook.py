a8b239fce00b2394628c2c8c1ea0929c5e30055a
Bruice1989 <zatikan89@gmail.com>
2026-05-19 21:40:11 +0300

    Update pre_build_hook.py

--- pre_build_hook.py
+++ pre_build_hook.py
@@ -1,13 +1,1 @@
-def post_build_arch(ctx, arch):
-    import shutil, os
-    # Видаляємо старий pygame з python-installs після збірки
-    pygame_path = os.path.join(
-        ctx.build_dir,
-        "python-installs",
-        "worldtitans",
-        arch.arch,
-        "pygame"
-    )
-    if os.path.exists(pygame_path):
-        shutil.rmtree(pygame_path)
-        print(f">>> Видалено старий pygame з python-installs: {pygame_path}")
+
