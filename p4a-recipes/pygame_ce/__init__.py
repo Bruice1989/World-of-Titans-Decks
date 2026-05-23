fc8a6c06b4eb7a2497b47e951f3727cbde5b6d6a
Bruice1989 <zatikan89@gmail.com>
2026-05-23 05:44:19 +0300

    Update __init__.py

--- p4a-recipes/pygame_ce/__init__.py
+++ p4a-recipes/pygame_ce/__init__.py
@@ -1,10 +1,21 @@
-from pythonforandroid.recipes.pygame import PygameRecipe
+from pythonforandroid.recipe import CythonRecipe
+from os.path import join
 
 
-class PygameCERecipe(PygameRecipe):
+class PygameCERecipe(CythonRecipe):
     version = '2.4.1'
     url = 'https://github.com/pygame-community/pygame-ce/archive/refs/tags/{version}.tar.gz'
     name = 'pygame_ce'
+    depends = ['sdl2', 'sdl2_image', 'sdl2_mixer', 'sdl2_ttf', 'python3']
+    call_hostpython_via_targetpython = False
+    install_in_hostpython = False
+    setup_extra_args = ['--ignore-setup-py']
+
+    def get_recipe_env(self, arch):
+        env = super().get_recipe_env(arch)
+        sdl2 = self.get_recipe('sdl2', self.ctx)
+        env['SDL_ROOT'] = sdl2.get_build_dir(arch.arch)
+        return env
 
 
 recipe = PygameCERecipe()
